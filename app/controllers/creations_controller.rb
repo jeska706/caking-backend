5eb5db3cf512aa6acaee3b784813f2e5f9b8554b
Jessica Beardsley <jessicabeardsley@hotmail.com>
2017-02-09 15:24:00 -0700

    Set up creation route

--- app/controllers/creations_controller.rb
+++ app/controllers/creations_controller.rb
@@ -3,49 +3,55 @@ class CreationsController < ApplicationController
 
   # GET /creations
   def index
-    @creations = Creation.all
+    @creations = Creation.where(user_id: params[:user_id])
 
     render json: @creations
   end
 
   # GET /creations/1
   def show
-    render json: @creation
+    creation = Creation.where(user_id: params[:user_id], id: params[:id])
+    render json: creation
   end
 
   # POST /creations
   def create
-    @creation = Creation.new(creation_params)
+    creation = Creation.new(creation_params)
+    creation.user_id = params[:user_id]
 
-    if @creation.save
-      render json: @creation, status: :created, location: @creation
+    if creation.save
+      render json: creation, status: 200
     else
-      render json: @creation.errors, status: :unprocessable_entity
+      render json: creation.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /creations/1
   def update
-    if @creation.update(creation_params)
-      render json: @creation
+    creation = Creation.where(user_id: params[:user_id])
+    creation.update(creation_params)
+
+    if creation.update(creation_params)
+      render json: creation
     else
-      render json: @creation.errors, status: :unprocessable_entity
+      render json: creation.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /creations/1
   def destroy
-    @creation.destroy
+    creation = Creation.where(user_id: params[:user_id])
+    creation.destroy(params[:id])
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_creation
-      @creation = Creation.find(params[:id])
+      creation = Creation.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
     def creation_params
-      params.require(:creation).permit(:title)
+      params.require(:creation).permit(:title, :user_id)
     end
 end
