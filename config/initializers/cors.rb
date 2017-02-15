9ebb1cf2e76198723b09e57f13f85179f1ed5e3e
jeska706 <jessicabeardsley@hotmail.com>
2017-02-15 10:53:59 -0700

    config cors 2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
 
-        origins 'https://caking.herokuapp.com/', 'localhost:2043', '*'
+        origins 'https://caking.herokuapp.com', 'localhost:2043', '*'
 
         resource '*',
             headers: :any,
