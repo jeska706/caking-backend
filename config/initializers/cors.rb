914e30826c070159abed6f88e8493e140f2d9e53
jeska706 <jessicabeardsley@hotmail.com>
2017-02-15 20:18:13 -0700

    changed protocal

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
 
-        origins 'localhost:2043', 'https://caking.herokuapp.com/'
+        origins 'localhost:2043', 'http://caking.herokuapp.com/'
 
         resource '*',
             headers: :any,
